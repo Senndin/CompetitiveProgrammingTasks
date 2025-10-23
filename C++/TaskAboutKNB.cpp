f08c45b3d662edc677d0ab9d88cddb362ef9e5d9
Denis Chernokur <chernokur.denys@hneu.net>
2025-10-24 01:07:19 +0300

    Added solve for KNB on C++

--- C++/TaskAboutKNB.cpp
+++ C++/TaskAboutKNB.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <iterator>
+
 
 using namespace std;
 
 int main() {
+     int r1, s1, p1;
+     cin >> r1 >> s1 >> p1;
+     int r2, s2, p2;
+     cin >> r2 >> s2 >> p2;
+     int countp = min(r1, s2) + min(s1, p2) + min(p1, r2);
+     cout << countp << endl;
      
 }
